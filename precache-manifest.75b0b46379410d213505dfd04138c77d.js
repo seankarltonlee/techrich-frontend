c23f0ce3dfefa6238a3427a58299dc23a69e9b0b
seankarltonlee <seankl@usc.edu>
2020-09-04 12:56:26 -0700

    Updates

--- precache-manifest.75b0b46379410d213505dfd04138c77d.js
+++ precache-manifest.75b0b46379410d213505dfd04138c77d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "213923e693971773d21bb8226c2932f4",
+    "revision": "38c4a152203f45c2bf45633aaa4e8db1",
     "url": "/index.html"
   },
   {
-    "revision": "81dfe23ec6807a1acef1",
+    "revision": "5aa8b640ae511c8e1ce3",
     "url": "/static/css/main.f23dea58.chunk.css"
   },
   {
-    "revision": "aeae560935d6c2916893",
-    "url": "/static/js/2.2bf28b8f.chunk.js"
+    "revision": "998bf6fe14d1db75da1d",
+    "url": "/static/js/2.1af47bda.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/static/js/2.2bf28b8f.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.1af47bda.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "81dfe23ec6807a1acef1",
-    "url": "/static/js/main.475a922c.chunk.js"
+    "revision": "5aa8b640ae511c8e1ce3",
+    "url": "/static/js/main.7f1f562f.chunk.js"
   },
   {
     "revision": "2747b914e9a60db2276f",
@@ -26,6 +26,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "fc664ed6812d4a4158e34e5040ad3654",
     "url": "/static/media/featured_image.fc664ed6.jpg"
+  },
+  {
+    "revision": "f03e6b9f07be69f5f4bf60e0e8833e2c",
+    "url": "/static/media/how-i-got-in-tech.f03e6b9f.md"
   },
   {
     "revision": "5f2160cbe0e369fb8bef5e444f01e401",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.88c699adae1f415e0ee8997f2b48729a.js"
+  "/precache-manifest.75b0b46379410d213505dfd04138c77d.js"
 );
 
 self.addEventListener('message', (event) => {
